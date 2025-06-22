8003b98e06b01de853db4f00ed115e1aeadb6a41
quotheJ <207427588+quotheJ@users.noreply.github.com>
2025-06-22 18:05:00 +0900

    表示方法の変更と整理

--- fisheye_lens.py
+++ fisheye_lens.py
@@ -70,39 +70,28 @@ y_lis_sorted = y_lis_np[sort_idx]
 x = np.arange(x_lis_sorted[0], x_lis_sorted[-1]+0.1, 0.1)
 f_sci=scipl.CubicSpline(x_lis_sorted,y_lis_sorted)
 
-# coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-1)
-# poly25 = np.poly1d(coeffs)
-# print(poly25)
-
-# coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-16)
-# poly10 = np.poly1d(coeffs)
-# print(poly10)
-
-coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-21)
-poly5 = np.poly1d(coeffs)
-print(f"poly5: {poly5}")
-
-coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-22)
-poly4 = np.poly1d(coeffs)
-print(poly4)
-
-coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-23)
-poly3 = np.poly1d(coeffs)
-print(poly3)
-
-
 plt.plot(x_lis_sorted, y_lis_sorted,'o')
-# plt.plot(x, f_sci(x),'-')
-# plt.plot(x, poly25(x), '--', label='Polynomial Fit 25')
-# plt.plot(x, poly10(x), '--', label='Polynomial Fit 10')
-plt.plot(x, poly5(x), '-', label='Polynomial Fit 5')
-plt.plot(x, poly4(x), '--', label='Polynomial Fit 4')
-plt.plot(x, poly3(x), '--', label='Polynomial Fit 3')
+
+legends = []
+legends.append('Row data')
+
+lines =["-", "--", "-.", ":"]
+
+def calculate_formula(poly):
+    coeffs = np.polyfit(x_lis_sorted, y_lis_sorted, deg=len(x_lis_sorted)-(len(x_lis_sorted)-poly))
+    equation = np.poly1d(coeffs)
+    print(f"poly{poly}: {equation}")
+    plt.plot(x, equation(x), lines[3%poly], label=f'Polynomial Fit {poly}')
+    legends.append(f'Polynomial Fit {poly}')
+
+calculate_formula(6)
+calculate_formula(5)
+calculate_formula(4)
+calculate_formula(3)
+calculate_formula(2)
+
+
 plt.xlabel('x [-]')
 plt.ylabel('y [-]')
-plt.legend(['Row data',
-            # 'Cubic Spline',
-            # 'Polynomial Fit25',
-            # 'Polynomial Fit10',
-            'Polynomial Fit5', 'Polynomial Fit4', 'Polynomial Fit3'])
+plt.legend(legends)
 plt.show()
